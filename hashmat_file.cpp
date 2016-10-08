21ada63268597cb9b7a49b1a6e4cd9796b6d5715
Giuliano Vallejo <giuliano.vallejo@yahoo.com>
2016-10-08 08:24:23 +0800

    Add files via upload

---
+++ hashmat_file.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<stdlib.h>
+#include <fstream>
+using namespace std;
+int main()
+{	int x,y,j,dif,diff,difff;
+	cout<<"Difference: "; 
+
+	 ifstream f("sample.txt");
+   
+    while (f >> x >> y) {
+    	cout<< abs(x-y)<<endl;
+    	
+    		
+}
+		     
+return 0;	
+}
